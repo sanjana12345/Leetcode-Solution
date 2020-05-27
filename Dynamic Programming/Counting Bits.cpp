3630fee73488529d355fb1eb3ca77cdb8889eb52
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 16:56:46 +0530

    https://leetcode.com/problems/counting-bits/

---
+++ Dynamic Programming/Counting Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+           int i;
+        for(i=0; i<=num; i++){ 
+        int cnt=0;
+        while(i>0){
+                i=(i&i+)
+                
+            }
+        }
+    }
+};

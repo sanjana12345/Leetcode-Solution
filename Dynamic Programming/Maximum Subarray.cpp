761fe6f76495bce50bbb45d9d5ab80e5fc0426f0
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 16:49:23 +0530

    https://leetcode.com/problems/maximum-subarray/

---
+++ Dynamic Programming/Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int sum = 0;
+        int max = nums[0];
+        for(int i = 0; i < nums.size(); i++){
+            sum+=nums[i];
+            if(nums[i]>=sum) 
+                sum = nums[i];
+       
+            max=max>sum? max:sum;
+        }
+        return max;
+    }
+};

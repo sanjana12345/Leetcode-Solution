7e8bff54f205fad649ad4cd187a60db53ae7cc65
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 16:07:34 +0530

    https://leetcode.com/problems/merge-intervals/

---
+++ Array/MergeIntervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return {};
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=ans[ans.size()-1][1]){
+                ans[ans.size()-1][1]=max(ans[ans.size()-1][1],intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    
+    }
+};

cbeb8fc7729c504ce0bd39e02e3d1e781278129e
sanjana Singh <sanjanasing214@gmail.com>
2020-06-03 05:18:50 +0530

    https://leetcode.com/problems/koko-eating-bananas/

---
+++ Binary Search/Koko Eating Bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int lo = 1, hi = pow(10, 9);
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (!possible(piles, H, mi))
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return lo;
+    }
+
+    bool possible(vector<int>& piles, int H, int K) {
+        int time = 0;
+        for (int p: piles)
+            time += (p - 1) / K + 1;
+        return time <= H;
+    }
+};
+};
